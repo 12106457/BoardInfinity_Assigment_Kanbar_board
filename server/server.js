e1d080213b613d82e02d103f9bff44b3f8526af7
12106457 <122795214+12106457@users.noreply.github.com>
2024-09-08 21:29:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors(
   {
-    origin:[],
+    origin:["https://kanban-board-frontend-mu.vercel.app/"],
     methods:["POST","GET","PUT","DELETE"],
     credentials: true
   }
