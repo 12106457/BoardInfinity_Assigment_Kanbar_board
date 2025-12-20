383bce2996ebea30ed58ed0e48051ee9f17e1374
Kedarisetti Sai <122795214+12106457@users.noreply.github.com>
2025-12-20 19:07:12 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <img src="https://cdn-icons-png.flaticon.com/512/762/762686.png" width={25} height={25} alt="Task managment icon" />
+        <h4>Task Management System</h4>
     </div>
   )
 }
